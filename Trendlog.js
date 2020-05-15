287ba27abf986c0fb32e154ef2b794461dcb7f2a
Bakke94 <58519183+Bakke94@users.noreply.github.com>
2020-05-15 16:16:45 +0200

    ændringer på sidemenu

--- Trendlog.js
+++ Trendlog.js
@@ -12,3 +12,31 @@ document.querySelectorAll('.button-dragndrop').forEach(function (element) {
     element.parentNode.removeChild(element);
     newParent.appendChild(element);
   }
+
+  $('.btn').click(function(){
+    $(this).toggleClass("click");
+    $('.sidebar').toggleClass("show");
+  });
+    $('.feat-btn').click(function(){
+      $('nav ul .feat-show').toggleClass("show");
+      $('nav ul .first').toggleClass("rotate");
+    });
+    $('.serv-btn').click(function(){
+      $('nav ul .serv-show').toggleClass("show1");
+      $('nav ul .second').toggleClass("rotate");
+    });
+    $('.stop-btn').click(function(){
+      $('nav ul .stop-show').toggleClass("show2");
+      $('nav ul .third').toggleClass("rotate");
+    });
+    $('.oee-btn').click(function(){
+      $('nav ul .oee-show').toggleClass("show3");
+      $('nav ul .four').toggleClass("rotate");
+    });
+    $('.sidst-btn').click(function(){
+      $('nav ul .sidst-show').toggleClass("show4");
+      $('nav ul .five').toggleClass("rotate");
+    });
+    $('nav ul li').click(function(){
+      $(this).addClass("active").siblings().removeClass("active");
+    });
