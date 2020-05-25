3e5ae6318dd11d6e4b81b404ccfe01b0cf7aa87b
Bakke94 <58519183+Bakke94@users.noreply.github.com>
2020-05-25 15:12:06 +0200

    fml

--- Trendlog.js
+++ Trendlog.js
@@ -5,12 +5,31 @@ document.querySelectorAll('.button-dragndrop').forEach(function (element) {
   })
   
   function toggleKeyword(event) {
-      var element = event.target;
-    var boxUpper = document.getElementById('inner-buttons-grid');
-    var boxPDF = document.getElementById('pdf-text');
-    var newParent = element.parentNode.id === 'inner-buttons-grid' ? boxPDF : boxUpper;
+    let element = event.target;
+    let boxUpper = document.getElementById('inner-buttons-grid');
+    let boxPDF = document.getElementById('pdf-text');
+    let newParent = element.parentNode.id === 'inner-buttons-grid' ? boxPDF : boxUpper;
     element.parentNode.removeChild(element);
     newParent.appendChild(element);
+  }
+
+  const buttons = document.querySelectorAll('button-dragndrop');
+  const buttonSpace = document.querySelectorAll('inner-buttons-grid', 'pdf-text');
+
+  let draggedItem = null;
+  for (let i = 0; i < buttons.length; i++) {
+    const item = buttons [i];
+
+    buttons.addEventListener('dragstart', function(event) {
+       console.log('dragstart', event);
+       draggedItem = this;
+    })
+
+    for (let j = 0; j < buttonSpace.length; j++) {
+      const list = buttonSpace [j];
+      
+    }
+    
   }
 
   $('.btn').click(function(){
